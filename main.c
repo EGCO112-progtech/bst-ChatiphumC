ecc9f870739cbf9b6554ee30dec465dea0c1f4c5
ChatiphumC <156408486+ChatiphumC@users.noreply.github.com>
2024-03-01 09:36:02 +0000

    TreePrint

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main( int argc, char **argv )
 
    // traverse the tree postOrder
 
-   printf("\nThe postnOrder traversal is:\n" );   
+   printf("\nThe postOrder traversal is:\n" );   
    postOrder( b.root );
 
    printf("\n");
